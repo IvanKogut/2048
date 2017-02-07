430470ec84b8b418b3a8cb7c4b423c3d1b07afae
John Kogut <kogut.iwan@gmail.com>
2017-02-08 00:27:48 +0200

    Add Tile class and modify Model class (add the playing field)

--- src/main/java/ua/kyiv/kpi/fpm/kogut/app/model/Model.java
+++ src/main/java/ua/kyiv/kpi/fpm/kogut/app/model/Model.java
@@ -4,4 +4,18 @@ package ua.kyiv.kpi.fpm.kogut.app.model;
  * Created by Admin on 07.02.2017.
  */
 public class Model {
+
+    private static final int FIELD_WIDTH = 4;
+
+    private Tile[][] gameTiles;
+
+    public Model() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
